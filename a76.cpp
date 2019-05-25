225c01bb4d6901e20612475ef3ec71b999e11846
summerlikey <summerlikey@gmail.com>
2019-05-25 17:17:37 +0800

    std::string

---
+++ a76.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+int main()
+{
+	std::string s1="dd";
+	std::string s2(s1);
+	std::cin>>s2;
+	std::cout<<"55555"<<std::endl;
+	std::string s3("Value");
+	//std::string s3 = "value";
+	std::string s4 (55,'c');
+
+	std::cout<<s3<<" "<<s4<<std::endl;
+	return 0;
+}
---
+++ a77.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string s;
+	std::string s2;
+	std::cin>>s;
+	std::cin>>s2;
+	std::cout<<s<<" "<<s2<<" ";
+	return 0;
+}
